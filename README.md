778a0079180d55f7d867fa2dd09dd9219ab375bf
Ferret <ferret@localhost.localdomain>
2017-03-15 15:47:28 +0100

    Misc

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 Just my Linux box setup / bootstrap scripts / dotfiles / etc
 
+### Install
+
+    git clone https://github.com/nathanchere/.ncx.git ~/.ncx
+    cd ~/.ncx
+   
+    chmod +777 *.sh
+
+    ./init_dotfiles.sh
+    ./init_fonts.sh
+    
+    ./install_[whatever].sh
+    
--- _.sh
+++ _.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Common config stuff
 
+#TODO:  2>&1 | tee logs/install.log
+
 # Exit on the first failure - it's bulletproof or GTFO
 set -e
 
@@ -8,15 +10,8 @@ set -e
 set -u
 
 # Tips from http://stackoverflow.com/a/25515370/243557
-
 yell() { echo "$0: $*" >&2; }
 die() { yell "$*"; exit 111; }
 try() { "$@" || die "cannot $*"; }
 
-NCXROOT = $(pwd)
-
-echo $NCXROOT
-
-yell()
-
-# TODO:  2>&1 | tee logs/install.log
+NCXROOT=$(pwd)
