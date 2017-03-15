315d02b38278823a7ba0cc4758eb478a0460f195
Nathan Chere <git@nathanchere.com.au>
2017-03-15 14:42:20 +0100

    Header/util script

---
+++ _.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Common config stuff
+
+# Exit on the first failure - it's bulletproof or GTFO
+set -e
+
+# Error on referencing any undefined variables (sanity check)
+set -u
+
+# Tips from http://stackoverflow.com/a/25515370/243557
+
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "cannot $*"; }
+
+NCXROOT = $(pwd)
+
+echo $NCXROOT
+
+yell()
