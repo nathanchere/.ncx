afb26608d662009dcbea53253c2dd1aa25a02929
Ferret <ferret@localhost.localdomain>
2017-03-17 12:16:51 +0100

    Change to hardlinking xsessions

--- init_sysconfig.sh
+++ init_sysconfig.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 . _.sh
 
-# System config is handled at root ("/") level rather than home
-# For this reason it's handled separately from normal dotfiles
 requireRoot
 
+installXsessions() {
+
+  # This is done with hard links instead of stow's symlinks due to permission
+  # issues. If the account running LightDM (or whatever DM) doesn't have
+  # permission to access wherever the repo is cloned to (i.e. specific user's
+  # home directory) it will not display the corresponding session options.
+
+  drawSubhead "Installing XSession .desktop files"
+
+  for SOURCEPATH in $NCXROOT/sysconfig/xsessions/*.*; do
+    FILENAME=$(basename $SOURCEPATH)
+    TARGETPATH="/usr/share/xsessions/$FILENAME"
+
+    if [ -f $TARGETPATH ]; then
+      echo "$FILENAME already exists as $TARGETPATH; skipping..."
+    else
+
+      if [ -L $TARGETPATH ]; then
+        echo "$FILENAME already symlinked; unlinking..."
+        unlink $TARGETPATH
+      fi
+
+      echo "Hard-linking $SOURCEPATH to $TARGETPATH"
+      ln $SOURCEPATH $TARGETPATH
+    fi
+  done
+}
+
+
 main() {
-  doStow xsessions sysconfig /
+  installXsessions
 }
 
 main 2>&1 |& tee -a $LOGFILE
