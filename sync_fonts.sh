630cdfe68dbbe449a0027a79be65b0f05a392425
Nathan Chere <git@nathanchere.com.au>
2017-03-21 17:31:46 +0100

    Better fonts handling

---
+++ sync_fonts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+. _.sh
+
+main() {
+  drawSubhead "Fixing font permissions"
+  chmod a+r -R ./fonts/
+
+  drawSubhead "Copying font files"
+  rsync -avm fonts/. "$HOME/.local/share/fonts"
+
+  drawSubhead "Updating system font cache"
+  fc-cache -fv
+}
+
+main 2>&1 |& tee -a "$LOGFILE"
