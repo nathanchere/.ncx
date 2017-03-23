94c1b69081841f71fa3c5fc4d8555365a5b33fc2
Nathan Chere <git@nathanchere.com.au>
2017-03-23 19:38:42 +0100

    Elixir install script from source

---
+++ install_elixir.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+. "./_.sh"
+
+requireRoot
+
+latestElixirSourceUrl() {
+  echo $(curl -sL "https://api.github.com/repos/elixir-lang/elixir/releases/latest" | grep tarball_url | cut -d '"' -f 4)
+}
+
+installedElixirVersion() {
+  echo $(rpm -qi elixir | grep "Version" | cut -d ':' -f 2 | cut -d ' ' -f 2)
+}
+
+latestElixirVersion() {
+  echo $(curl -sL "https://api.github.com/repos/elixir-lang/elixir/releases/latest" | grep tag_name | cut -d 'v' -f 2 | cut -d '"' -f 1)
+}
+
+main() {
+  drawSubhead "Checking installed vs latest Elixir versions"
+  INSTALLEDVERSION=$(installedElixirVersion)
+  LATESTVERSION=$(latestElixirVersion)
+
+  if [ "$INSTALLEDVERSION" == "$LATESTVERSION" ]; then
+    echo "Latest version $LATESTVERSION already installed; skipping..."
+  else
+    if [ "$INSTALLEDVERSION" == "" ]; then
+      echo "No previous Elixir installation detected; installing $LATESTVERSION"
+    else
+      echo "Outdated version $INSTALLEDVERSION installed; updating to $LATESTVERSION"
+    fi
+
+    echo "Installing prerequisites"
+    dnf install -y erlang
+
+    DOWNLOADURL=$(latestElixirSourceUrl)
+    INSTALLER="$TMPROOT/elixir-$LATESTVERSION.tar.gz"
+    SOURCEDIR="$TMPROOT/elixir-src"
+    rm -f "$INSTALLER"
+    rm -rf "$SOURCEDIR"
+    echo "Downloading from $DOWNLOADURL to $INSTALLER"
+    download "$(latestElixirSourceUrl)" "$INSTALLER"
+
+    echo "Extracting source tarball to $SOURCEDIR"
+    mkdir -p "$SOURCEDIR"
+    tar -zvxf "$INSTALLER" -C "$SOURCEDIR"
+
+    echo "Building Elixir from source"
+    cd "$SOURCEDIR"
+    cd *
+    make clean compile install
+  fi
+}
+
+main 2>&1 |& tee -a "$LOGFILE"
