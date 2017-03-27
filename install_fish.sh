d376bde0e76f374d100b2016dccfcec833a4de75
Nathan Chere <git@nathanchere.com.au>
2017-03-27 02:53:17 +0200

    Fish installer

--- install_fish.sh
+++ install_fish.sh
@@ -5,9 +5,11 @@
 requireRoot
 
 main() {
-
-  curl -L https://get.oh-my.fish | fish
-
+  dnf install -y fish
+  
+  INSTALLER="$TMPROOT/ohmy.fish"
+  curl -L https://get.oh-my.fish > "$INSTALLER"
+  /usr/bin/fish "$INSTALLER" --path="$HOME/.local/share/omf" --config="$HOME/.config/omf"
 }
 
 main 2>&1 |& tee -a "$LOGFILE"
