47bccd4b40ed8f2bb8d13e5407a0330b3a54f9e2
Nathan Chere <git@nathanchere.com.au>
2017-03-27 13:48:27 +0200

    Misc cleanups

--- install_asdf.sh
+++ install_asdf.sh
@@ -19,23 +19,11 @@ installAsdf() {
   currentRelease=$(git describe --tags `git rev-list --tags --max-count=1`)
   git checkout "$currentRelease"
 
-  cd -
+  chmod a+rwx -R "$ASDFPATH"
 }
 
 main() {
-
   installAsdf
-
-  drawSubhead "Installing Elixir stuff"
-  dnf install -y erlang
-  . "./install.elixir.sh"
-
-  drawSubhead "Installing Ruby stuff"
-  dnf install -y ruby gem
-
-  drawHead "Installing Python stuff"
-  #curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-  #pyenv update # TODO: add to fish, not just bash
 }
 
 main 2>&1 |& tee -a "$LOGFILE"
--- install_fish.sh
+++ install_fish.sh
@@ -6,10 +6,26 @@ requireRoot
 
 main() {
   dnf install -y fish
-  
-  INSTALLER="$TMPROOT/ohmy.fish"
-  curl -L https://get.oh-my.fish > "$INSTALLER"
-  /usr/bin/fish "$INSTALLER" --path="$HOME/.local/share/omf" --config="$HOME/.config/omf"
+
+  OMFPATH="$HOME/.local/share/omf"
+
+  if [! -d "$OMFPATH" [; then
+    INSTALLER="$TMPROOT/ohmy.fish"
+    curl -L https://get.oh-my.fish > "$INSTALLER"
+    /usr/bin/fish "$INSTALLER" --path="$OMFPATH" --config="$HOME/.config/omf"
+  else
+    echo "OhMyFish already installed; skipping..."
+  fi
+
+  chmod a+rw -R "$OMFPATH"
+
+  rm -f "$HOME/.fishrc"
+  rm -rf "$HOME/.config/fish"
+  rm -rf "$HOME/.config/omf"
+
+  doStow fish dotfiles "$HOME"
+
+  echo "Don't forget to chsh to set fish as your default shell"
 }
 
 main 2>&1 |& tee -a "$LOGFILE"
