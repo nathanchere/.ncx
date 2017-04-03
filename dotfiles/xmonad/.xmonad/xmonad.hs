02818f8bc1051273d381c8788172fbdd951eda60
Nathan Chere <git@nathanchere.com.au>
2017-04-03 15:25:38 +0200

    Add mirror/swap toggle for xmonad

--- dotfiles/xmonad/.xmonad/xmonad.hs
+++ dotfiles/xmonad/.xmonad/xmonad.hs
@@ -1,7 +1,11 @@
 import XMonad
 import XMonad.Config.Desktop
 import XMonad.Layout.Spacing
+import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
+
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.Reflect
 
 myTerminal = "terminator"
 myModMask = mod4Mask
@@ -21,15 +25,19 @@ myBorderWidth = 3
 myFocusedBorderColor = "#BBFF00"
 myNormalBorderColor = "#778877"
 
-myLayoutHook = spacingWithEdge 4
+myLayoutHook = spacingWithEdge mySpacingWidth
+  $ avoidStruts
+  $ mkToggle (single REFLECTX)
+  $ mkToggle (single REFLECTY)
   $ layoutHook desktopConfig
 
 -- TODO: workspaces 9..16 for minimised windows
 myWorkspaces = ["1:\xf121","2:\xf02d","3:code", "4:misc"] ++ map show [5..8]
 
 myKeys =
-	[(("M4-l"), runOrRaise "lock" (className =? "Firefox"))	
-	,(("M4-r"), spawn "rofi --show")
+	--[(("M4-l"), runOrRaise "lock" (className =? "Firefox"))
+  [(("M4-m"), sendMessage $ Toggle REFLECTX)
+	,(("M4-r"), spawn "rofi -show")
 	]
 
 
@@ -44,7 +52,7 @@ myConfig = desktopConfig
   , layoutHook = myLayoutHook
   , workspaces = myWorkspaces
   } `additionalKeysP` myKeys
-  
+
 --  `additionalKeysP`
 --  [ ("M-r", )
 --  , ("M-S-k", kill)
