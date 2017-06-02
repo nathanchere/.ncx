40e427d24d1a420951f73856f6afe44e525445fe
Nathan Chere <git@nathanchere.com.au>
2017-06-02 08:10:23 +0200

    Fix broken sync_sessions

--- sync_xsessions.sh
+++ sync_xsessions.sh
@@ -6,10 +6,10 @@ requireRoot
 
 main() {
   drawSubhead "Fixing xsession permissions"
-  chmod a+r -R ./sysconfig/xsessions/
+  chmod a+r -R ./system/xsessions/
 
   drawSubhead "Copying xsession files"
-  rsync -avmi sysconfig/xsessions/. "/usr/share/xsessions"
+  rsync -avmi system/xsessions/. "/usr/share/xsessions"
 }
 
 main 2>&1 |& tee -a "$LOGFILE"
