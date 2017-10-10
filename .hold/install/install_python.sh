5f5b653820fb4d9d1c09667d1478b58ac8f0eff9
Nathan Chere <git@nathanchere.com.au>
2017-10-10 17:17:20 +0200

    Placeholder for python installer

---
+++ .hold/install/install_python.sh
@@ -0,0 +1,7 @@
+# curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+# pyenv update
+# install latest 2.x and 3.x
+
+
+# dnf install zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz xz-devel
+# build depnedencies
